d2474bc2098b90a23fdcfd802dfb2b8841083eeb
Josue Morales <molinajosue92@hotmail.com>
2023-02-19 14:39:33 -0500

    add files

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+use std::{fs::{File, self}, mem};
+
+use tree_sitter::{Parser, InputEdit};
+
+const KOTLIN_FILE: &str = include_str!("stack.kt");
+
+fn main() {
+    let mut parser = Parser::new();
+    parser.set_language(tree_sitter_kotlin::language()).unwrap();
+    let parsed = parser.parse(KOTLIN_FILE, None).unwrap();
+    let root_node = parsed.root_node();
+    let mut cursor = parsed.walk();
+    fs::copy("./src/stack.kt", "./stack_mutate.kt").unwrap();
+    search_children(root_node, &mut cursor, " ", false);
+}
+
+fn search_children(
+    root: tree_sitter::Node, 
+    cursor: &mut tree_sitter::TreeCursor, 
+    prefix: &str,
+    parent_was_comp_exp: bool
+) {
+    root
+        .children(&mut cursor.clone())
+        .for_each(|node| {
+            if parent_was_comp_exp && node.kind() == "<" {
+                let new_op = ">".as_bytes();
+                let mut kt_file: Vec<u8> = KOTLIN_FILE.as_bytes().iter().map(|b| *b).collect();
+                for (i, b) in kt_file.iter_mut().skip(node.start_byte()).enumerate() {
+                    if i >= (node.end_byte() - node.start_byte()) {
+                        break;
+                    }
+                    println!("{} - {}", i, node.end_byte() - node.start_byte());
+                    *b = new_op[i];
+                }
+                fs::write("./stack_mutate.kt", kt_file).unwrap();
+                // kt_file[0] = ">".as_bytes();
+                // kt_file.
+                // let edit = InputEdit {
+                //     start_byte: ,
+                //     old_end_byte: todo!(),
+                //     new_end_byte: todo!(),
+                //     start_position: todo!(),
+                //     old_end_position: todo!(),
+                //     new_end_position: todo!(),
+                // };
+                // node.edit(&edit);
+                println!("{}({} {} - {})", prefix, node.kind(), node.start_byte(), node.end_byte());
+            }
+            search_children(
+                node, 
+                &mut cursor.clone(), 
+                &format!("    {}", prefix),
+                node.kind() == "comparison_expression" || node.kind() == "equality_expression"
+            )
+        })
+}
---
+++ src/stack.kt
@@ -0,0 +1,59 @@
+/*
+fun main() {
+	// Given a string containing brackets of 4 types: (), [], {}, <>
+	// Check whether brackets are in the correct sequence, ignore all other characters
+	var input = """
+    (a+[b*c]-{d/3})
+    (a + [b * c) - 17]
+    (((a * x) + [b] * y) + c
+    auf(zlo)men [gy<psy>] four{s}
+    """.trimIndent()
+
+    // split the string on \n
+    var lines = input.split('\n')
+    var res = mutableListOf<Int>()
+    for (line in lines) {
+        res.add(isBracketSequenceCorrect(line)) 
+    }
+    println(res.joinToString(" "))
+}
+
+fun isBracketSequenceCorrect(line: String): Int {
+    var stack = mutableListOf<Char>()
+    val bracketPairs = mapOf<Char, Char>('{' to '}', '(' to ')', '{' to '}', '<' to '>', '[' to ']')
+    for (char in line) {
+        if (!(char in bracketPairs.keys) && !(char in bracketPairs.values)) continue
+        if (stack.isEmpty()) {
+            stack.add(char)
+            continue
+        }
+        when (char) {
+            in bracketPairs.keys -> {
+                // keys is the opening
+                stack.add(char)
+            }
+            in bracketPairs.values -> {
+                // get the top of the stack & get its closing pair
+                val pair = bracketPairs.getOrDefault(stack.last(), ' ')
+                if (char != pair) {
+                    return 0
+                }
+                stack.removeAt(stack.lastIndex)
+            }
+        }
+        
+    }
+    return if (stack.isEmpty()) 1 else 0
+}
+*/
+fun comparisons() {
+    val x = 10
+    val y = 11
+    if (x < y) {
+        println("X is less than Y")
+    }
+    
+    if (x == y) {
+        println("X equals Y")
+    }
+}
